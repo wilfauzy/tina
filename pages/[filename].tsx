dcb833bbf01e309820c8cc288e73b174c58b34e2
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:44:11 +0700

    Initialize tina-preview-plugin-no-encoding

---
+++ pages/[filename].tsx
@@ -0,0 +1,51 @@
+import { Blocks } from "../components/blocks-renderer";
+import { useTina } from "tinacms/dist/react";
+import { Layout } from "../components/layout";
+import { client } from "../tina/__generated__/client";
+import { expandWithMetadata } from "@tinacms/preview-helpers";
+import { useEditOpen } from "@tinacms/preview-helpers/dist/react";
+
+export default function HomePage(
+  props: AsyncReturnType<typeof getStaticProps>["props"]
+) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+  useEditOpen("/admin");
+  return (
+    <Layout rawData={data} data={data.global as any}>
+      <Blocks {...data.page} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  let tinaProps = await client.queries.contentQuery({
+    relativePath: `${params.filename}.md`,
+  });
+  if (process.env.VERCEL_ENV === "preview") {
+    tinaProps = await expandWithMetadata(tinaProps, client, true);
+  }
+  return {
+    props: {
+      data: tinaProps.data,
+      query: tinaProps.query,
+      variables: tinaProps.variables,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const pagesListData = await client.queries.pageConnection();
+  return {
+    paths: pagesListData.data.pageConnection.edges.map((page) => ({
+      params: { filename: page.node._sys.filename },
+    })),
+    fallback: false,
+  };
+};
+
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+  T extends (...args: any) => Promise<infer R> ? R : any;
