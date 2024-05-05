ee89c18d0687efee37fa696b7dc072fa37395724
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:43:59 +0700

    Initialize self-hosted-db-connection-demo

---
+++ .tina/database.js
@@ -0,0 +1,106 @@
+import { createDatabase } from "@tinacms/graphql";
+import { FilesystemBridge } from "@tinacms/datalayer";
+import { MongodbLevel } from "mongodb-level";
+import { config } from "dotenv";
+import { Octokit } from "@octokit/rest";
+import { Base64 } from "js-base64";
+
+const owner = process.env.GITHUB_OWNER;
+const repo = process.env.GITHUB_REPO;
+const token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
+const branch = process.env.GITHUB_BRANCH;
+
+const octokit = new Octokit({
+  auth: token,
+});
+
+const onPut = async (key, value) => {
+  let sha;
+  try {
+    const {
+      data: { sha: existingSha },
+    } = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: key,
+      branch,
+    });
+    sha = existingSha;
+  } catch (e) {}
+
+  const { data } = await octokit.repos.createOrUpdateFileContents({
+    owner,
+    repo,
+    path: key,
+    message: "commit from self-hosted tina",
+    content: Base64.encode(value),
+    branch,
+    sha,
+  });
+};
+const onDelete = async (key) => {
+  let sha;
+  try {
+    const {
+      data: { sha: existingSha },
+    } = await octokit.repos.getContent({
+      owner,
+      repo,
+      path: key,
+      branch,
+    });
+    sha = existingSha;
+  } catch (e) {}
+  if (sha) {
+    const { data } = await octokit.repos.deleteFile({
+      owner,
+      repo,
+      path: key,
+      message: "commit from self-hosted tina",
+      branch,
+      sha,
+    });
+    console.log("data", data);
+  }
+};
+
+config();
+
+const level = new MongodbLevel({
+  mongoUri: process.env.MONGO_URI,
+  dbName: "tinacms",
+  collectionName: "tinacms",
+});
+
+// export default createDatabase({
+//   bridge: new FilesystemBridge(process.cwd()),
+//   level,
+//   onPut,
+//   onDelete,
+// });
+
+export class MockBridge {
+  constructor() {}
+  async glob(pattern, extension) {
+    return [];
+  }
+  supportsBuilding() {
+    return false;
+  }
+  async delete(filepath) {}
+  async get(filepath) {
+    throw new Error("not supported!");
+
+    return "";
+  }
+  async putConfig(filepath, data) {}
+  async put(filepath, data) {}
+}
+
+export default createDatabase({
+  bridge: new FilesystemBridge(process.cwd()),
+  level,
+  // Uncomment this and provide your access token to the GITHUB_PERSONAL_ACCESS_TOKEN env var
+  // onPut,
+  // onDelete,
+});
