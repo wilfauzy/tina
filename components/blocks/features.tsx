fe1644987b41b752a2bf08b2afd7ef1219b75003
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:44:38 +0700

    Initialize fix/size-issue

---
+++ components/blocks/features.tsx
@@ -0,0 +1,64 @@
+import { Section } from "../util/section";
+import { Container } from "../util/container";
+import { Icon } from "../util/icon";
+import {
+  PageBlocksFeatures,
+  PageBlocksFeaturesItems,
+} from "../../tina/__generated__/types";
+import { tinaField } from "tinacms/dist/react";
+
+export const Feature = ({
+  featuresColor,
+  data,
+}: {
+  featuresColor: string;
+  data: PageBlocksFeaturesItems;
+}) => {
+  return (
+    <div
+      data-tina-field={tinaField(data)}
+      className="flex-1 flex flex-col gap-6 text-center items-center lg:items-start lg:text-left max-w-xl mx-auto"
+      style={{ flexBasis: "16rem" }}
+    >
+      {data.icon && (
+        <Icon
+          tinaField={tinaField(data, "icon")}
+          parentColor={featuresColor}
+          data={{ size: "large", ...data.icon }}
+        />
+      )}
+      {data.title && (
+        <h3
+          data-tina-field={tinaField(data, "title")}
+          className="text-2xl font-semibold title-font"
+        >
+          {data.title}
+        </h3>
+      )}
+      {data.text && (
+        <p
+          data-tina-field={tinaField(data, "text")}
+          className="text-base opacity-80 leading-relaxed"
+        >
+          {data.text}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export const Features = ({ data }: { data: PageBlocksFeatures }) => {
+  return (
+    <Section color={data.color}>
+      <Container
+        className={`flex flex-wrap gap-x-10 gap-y-8 text-left`}
+        size="large"
+      >
+        {data.items &&
+          data.items.map(function (block, i) {
+            return <Feature featuresColor={data.color} key={i} data={block} />;
+          })}
+      </Container>
+    </Section>
+  );
+};
