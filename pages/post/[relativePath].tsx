d9192598ad8dde760703f4a5804f1d473df7c9d9
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:43:39 +0700

    Initialize handle-nested-docs

---
+++ pages/post/[relativePath].tsx
@@ -0,0 +1,59 @@
+import { Post } from "../../components/posts/post";
+import { client } from "../../.tina/__generated__/client";
+import { useTina } from "tinacms/dist/edit-state";
+import { Layout } from "../../components/layout";
+
+// Use the props returned by get static props
+export default function BlogPostPage(
+  props: AsyncReturnType<typeof getStaticProps>["props"]
+) {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+  if (data && data.post) {
+    return (
+      <Layout rawData={data} data={data.global as any}>
+        <Post {...data.post} />;
+      </Layout>
+    );
+  }
+  return (
+    <Layout>
+      <div>No data</div>;
+    </Layout>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const tinaProps = await client.queries.blogPostQuery({
+    relativePath: `${params.relativePath}.mdx`,
+  });
+  return {
+    props: {
+      ...tinaProps,
+    },
+  };
+};
+
+/**
+ * To build the blog post pages we just iterate through the list of
+ * posts and provide their "relativePath" as part of the URL path
+ *
+ * So a blog post at "content/posts/hello.md" would
+ * be viewable at http://localhost:3000/posts/hello
+ */
+export const getStaticPaths = async () => {
+  const postsListData = await client.queries.postConnection();
+
+  return {
+    paths: postsListData.data.postConnection.edges.map((post) => ({
+      params: { relativePath: post.node._sys.breadcrumbs.join("/") },
+    })),
+    fallback: "blocking",
+  };
+};
+
+export type AsyncReturnType<T extends (...args: any) => Promise<any>> =
+  T extends (...args: any) => Promise<infer R> ? R : any;
