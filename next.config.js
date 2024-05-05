9289544d6ce48e3c2bc13d6095aae1af800cfdd1
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:43:50 +0700

    Initialize next-config-redirect

---
+++ next.config.js
@@ -0,0 +1,66 @@
+const { redirect } = require("next/dist/server/api-utils");
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/",
+          destination: "/home",
+        },
+        {
+          source: "/admin",
+          destination: "/admin/index.html",
+        },
+      ],
+      // afterFiles: [
+      //   {
+      //     source: "/:path*",
+      //     has: [
+      //       {
+      //         type: "header",
+      //         key: "sec-fetch-dest",
+      //         value: "document",
+      //       },
+      //       {
+      //         type: "cookie",
+      //         key: "tina_auth",
+      //         value: "true",
+      //       },
+      //     ],
+      //     destination: "http://localhost:3000/admin?adminroute=#/~/:path*",
+      //   },
+      // ],
+    };
+  },
+  async redirects() {
+    return [
+      {
+        has: [
+          {
+            type: "header",
+            key: "sec-fetch-dest",
+            value: "document",
+          },
+          {
+            type: "cookie",
+            key: "tina-edit",
+            value: "true",
+          },
+        ],
+        source: "/:path((?!admin$).*)",
+        destination: "/admin?redirect=/#/~/:path*",
+        permanent: false,
+      },
+    ];
+  },
+};
