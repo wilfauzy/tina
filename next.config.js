be9c716b53103ef12bd29dce2bd29e61ad57223f
Wildan Fauzy <nadliwis@gmail.com>
2024-05-05 13:43:41 +0700

    Initialize fix/dep-vulnerabilities

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+};
